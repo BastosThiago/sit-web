667136e1819b0e00a49ae6b7e4120964b5de4853
thiago.bastosinfo@gmail.com <thiago.bastosinfo@gmail.com>
2019-08-28 18:17:52 -0300

    Configuração para deploy no Heroku

--- sistema_treinamentos/settings.py
+++ sistema_treinamentos/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'sit-webr.herokuapp.com',]
 
 
 # Application definition
@@ -129,5 +129,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
